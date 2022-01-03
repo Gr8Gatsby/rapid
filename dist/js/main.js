11788a1203f9dde66a1b0afb82765ff812ac28db
Kevin Hill <kevinhill@live.com>
2022-01-02 20:18:46 -0800

    feat: adding random fact to web page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,22 @@
 /* TODO: add code */
+function getRandomFact() {
+  fetch("https://numbersapi.p.rapidapi.com/random/trivia?min=1&max=25&fragment=true&json=true", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "numbersapi.p.rapidapi.com",
+      "x-rapidapi-key": "7cf9911ea0msh906fb923a0b1080p190b95jsn263f33e084a8"
+    }
+  }).then(function (response) {
+    return response.text();
+  }).then(function (data) {
+    var info = JSON.parse(data);
+    document.querySelector('[name="number"]').textContent = info.number;
+    document.querySelector('[name="text"]').textContent = info.text;
+  })["catch"](function (err) {
+    console.error(err);
+  });
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  getRandomFact();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,11 @@ function getRandomFact() {
             "x-rapidapi-key": "7cf9911ea0msh906fb923a0b1080p190b95jsn263f33e084a8"
         }
     }).then(response => {
-        console.log(response);
+        return response.text();
+    }).then(data => {
+        let info = JSON.parse(data);
+        document.querySelector('[name="number"]').textContent = info.number;
+        document.querySelector('[name="text"]').textContent = info.text;
     }).catch(err => {
         console.error(err);
     });
